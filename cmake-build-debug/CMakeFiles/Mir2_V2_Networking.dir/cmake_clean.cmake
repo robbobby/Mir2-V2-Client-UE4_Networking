7ea7e86aee879ba0ef63a4ecf9541a81a3da2539
robbobby <69080884+robbobby@users.noreply.github.com>
2021-11-10 20:45:52 +0000

    Starting on CMake

---
+++ cmake-build-debug/CMakeFiles/Mir2_V2_Networking.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/Mir2_V2_Networking.dir/main.cpp.o"
+  "Mir2_V2_Networking"
+  "Mir2_V2_Networking.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/Mir2_V2_Networking.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ cmake-build-debug/CMakeFiles/Mir2_V2_Networking.dir/depend.make
@@ -0,0 +1,2 @@
+# Empty dependencies file for Mir2_V2_Networking.
+# This may be replaced when dependencies are built.
